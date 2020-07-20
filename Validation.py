20e38055175e30976712563d6b9cf728e7230df5
JulietRowe <64859123+JulietRowe@users.noreply.github.com>
2020-07-20 12:35:23 -0700

    Current
    Bar plot, bland altman plot and ICC plotly table

--- Validation.py
+++ Validation.py
@@ -18,18 +18,18 @@ get_ipython().magic('clear')
 sg.theme('LightBlue2')
 
 sg.popup_ok('The following program was designed to analyze athlete data for the validation of timing gates', 
-           title = 'Timing Gate Validation')
+            title = 'Timing Gate Validation')
 sg.popup_ok_cancel('This program will prompt you to upload a .csv file. Follow these instructions carefully as everything is case sensitive! This file MUST be formatted in the following way:',
-           ' - Five columns', ' - One row containing column names',
-           ' - Column names: Athlete, Trial, Radar, TimingGate, Optojump',
-           title = 'Instructions')
+            ' - Five columns', ' - One row containing column names',
+            ' - Column names: Athlete, Trial, Radar, TimingGate, Optojump',
+            title = 'Instructions')
 
 #Select .csv file for analysis 
 File = sg.popup_get_file('Please select the .csv file for analyzing')
 data = pd.read_csv(File, header = 0, keep_default_na = False)
 
-#data = pd.read_csv('C:/Users/julie/GitHub/Validation/VelocityDataNew.csv',header = 0,
-              #  keep_default_na = False) #Keep athlete 'NA' from becoming NaNs
+# data = pd.read_csv('C:/Users/julie/GitHub/Validation/VelocityDataNew.csv',header = 0,
+#                 keep_default_na = False) #Keep athlete 'NA' from becoming NaNs
 
 #Sorting the data by athlete and trial in ascending order
 data.sort_values(['Athlete', 'Trial'], inplace = True, ascending = [True, True])
@@ -92,6 +92,54 @@ ICCOpto.sort_values('Athlete', inplace = True, ascending = True)
 
 iccRadar = pg.intraclass_corr(data = ICCRadar, targets = 'variable', raters = 'Athlete', ratings = 'value'  )
 iccOpto = pg.intraclass_corr(data = ICCOpto, targets = 'variable', raters = 'Athlete', ratings = 'value'  )
+iccRadar = iccRadar.round(decimals = 2)
+iccOpto = iccOpto.round(decimals = 2)
+iccRadar = iccRadar.drop("Description", axis = 1)
+iccOpto = iccOpto.drop("Description", axis = 1)
 
-print(iccRadar)
-print(iccOpto)
+from plotly.offline import plot
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+
+# fig4, ax = plt.subplots(figsize = (16, 2))
+# ax.axis('off')
+# ax.axis('tight')
+# ax.set_frame_on(False)
+
+# ax.table(cellText = iccRadar.values, 
+#           rowLabels = iccRadar.index,
+#           colLabels = iccRadar.columns,
+#           loc = "center")
+
+# ax.set_title("Timing and Radar ICC values")
+# fig4.tight_layout()
+
+
+table1 = ff.create_table(iccRadar)
+table2 = ff.create_table(iccOpto)
+fig = make_subplots(rows = 2, 
+                    cols = 1,
+                    print_grid = True,
+                    vertical_spacing = 0.085,
+                    subplot_titles = ('ICC values for Timing Gates and Radar', 'ICC values for Timing Gate and Optojump'))
+
+fig.add_trace(table1.data[0], 1, 1)
+fig.add_trace(table2.data[0], 2, 1)
+fig.layout.xaxis.update(table1.layout.xaxis)
+fig.layout.yaxis.update(table1.layout.yaxis)
+fig.layout.xaxis2.update(table2.layout.xaxis)
+fig.layout.yaxis2.update(table2.layout.yaxis)
+
+for k in range(len(table2.layout.annotations)):
+    table2.layout.annotations[k].update(xref = 'x2', yref = 'y2')
+
+all_annots = fig.layout.annotations+table1.layout.annotations + table2.layout.annotations
+fig.layout.annotations = all_annots
+
+fig.layout.update(width=800, height=600, margin=dict(t=100, l=50, r=50, b=50));
+
+plot(fig, auto_open = True)
+
+
+    
