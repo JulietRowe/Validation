240d75a970d7ced97c7247e389b067e4b694fea2
JulietRowe <64859123+JulietRowe@users.noreply.github.com>
2020-07-30 15:28:59 -0700

    Code for validation

--- Validation.py
+++ Validation.py
@@ -8,6 +8,12 @@ Created on Tue Jul 14 14:03:45 2020
 import pandas as pd
 import PySimpleGUI as sg
 import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import pingouin as pg
+from plotly.offline import plot
+from plotly.subplots import make_subplots
+import plotly.figure_factory as ff
 
 #Popup window with instructions 
 sg.theme('LightBlue2')
@@ -27,8 +33,6 @@ NewData[['Optojump_Max', 'Optojump_Avg']] = data.groupby('Athlete')['Optojump'].
 NewData.reset_index(inplace = True)
 
 #Visualizing data 
-import matplotlib.pyplot as plt
-
 fig1, axes = plt.subplots(2, figsize =(8,5), dpi = 300)
 fig1.suptitle('Instantaneous and Average Max Velocities', fontweight = "bold", size = 14)
 NewData.plot(x = "Athlete", y = ["Radar_Max", "TimingGate_Max", "Optojump_Max"], ax = axes[0],
@@ -51,7 +55,6 @@ for ax in axes.flat:
 plt.show()
     
 #Bland Altman plots
-import statsmodels.api as sm
 fig2, ax = plt.subplots(1, figsize = (8,5), dpi = 300)
 sm.graphics.mean_diff_plot(NewData['TimingGate_Max'], NewData['Radar_Max'], ax = ax)
 plt.title('Timing Gate and Radar Bland Altman plot', fontsize = 18)
@@ -63,7 +66,6 @@ plt.title('Timing Gate and Optojump Bland Altman plot', fontsize = 18)
 plt.show()  
 
 #ICC table
-import pingouin as pg
 ICCRadar = pd.DataFrame()
 ICCRadar['Athlete'] = NewData['Athlete']
 ICCRadar['TimingGate'] = NewData['TimingGate_Max']
@@ -84,10 +86,6 @@ iccRadar = iccRadar.round(decimals = 2)
 iccOpto = iccOpto.round(decimals = 2)
 iccRadar = iccRadar.drop("Description", axis = 1)
 iccOpto = iccOpto.drop("Description", axis = 1)
-
-from plotly.offline import plot
-from plotly.subplots import make_subplots
-import plotly.figure_factory as ff
 
 table1 = ff.create_table(iccRadar)
 table2 = ff.create_table(iccOpto)
